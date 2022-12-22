6c428b657662dd03daae26265d71d33b3ac4d155
nurulcse7 <nurul.cse7@gmail.com>
2022-12-23 00:57:59 +0600

    Services shorting and searching implement

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import useTitle from '../../../hooks/useTitle';
 import ServiceCard from './ServiceCard';
 
 const Services = () => {
-  useTitle('Services')
+  useTitle('Services');
   const [services, setServices] = useState([]);
-
+  const [isAsc, setIsAsc] = useState(true);
+  const [search, setSearch] = useState('');
+  const searchRef = useRef();
   useEffect(() => {
-    fetch('http://localhost:5000/services')
+    fetch(
+      `http://localhost:5000/services?search=${search}&order=${
+        isAsc ? 'asc' : 'desc'
+      }`
+    )
       .then((res) => res.json())
       .then((data) => setServices(data));
-  }, []);
+  }, [isAsc, search]);
 
+  const handleSearch = () => {
+    setSearch(searchRef.current.value);
+  };
   return (
     <div>
       <div className='text-center mb-4 shadow-2xl'>
@@ -25,6 +34,24 @@ const Services = () => {
           works for you. We like to send newsletters on behalf of our clients
           twice a month.
         </p>
+        <div className='p-4 flex justify-end '>
+          <input
+            ref={searchRef}
+            type='text'
+            placeholder='Type here'
+            className='input input-bordered input-primary w-1/4 max-w-xs'
+          />
+          <button className='btn btn-primary mr-5 capitalize text-lg' onClick={handleSearch}>
+            Search
+          </button>
+          <button
+            className='btn btn-secondary tooltip tooltip-left capitalize text-lg'
+            onClick={() => setIsAsc(!isAsc)}
+            data-tip='Show service as per price low/high'
+          >
+            {isAsc ? 'desc' : 'asc'}
+          </button>
+        </div>
       </div>
       <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
         {services.map((service) => (
